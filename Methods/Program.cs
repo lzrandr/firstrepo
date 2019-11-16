cec0535b6c038429b0582ae30db98dc804fdb696
Andrei L <lazar.andrei@outlook.com>
2019-11-16 12:17:43 +0200

    intial commit

---
+++ Methods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //  int sum = Sum();
+            // Console.WriteLine(Sum());
+            // Sum();
+
+
+          //  int first = 7;
+         //   int second = 10;
+         //   Console.WriteLine(Multiply(first, second));
+
+        //    Multiply(4, 5);
+
+            DisplayNumbers(10);
+        }
+
+        static void Sum()
+        {
+            int a= 2;
+            int b= 3;
+            int sum = a + b;
+            Console.WriteLine(sum);
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static void DisplayNumbers(int a)
+        {
+            
+            for (int i = 1; i <= a; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
